c20ee9f0952f009418efd20992e7de953426edf2
Namima1 <129132573+Namima1@users.noreply.github.com>
2023-11-30 11:48:25 -0500

    Add files via upload

---
+++ Section_4_Object_methods.rb
@@ -0,0 +1,12 @@
+actor = "Stallone"
+
+# Use a method to determine the number of characters in 
+# the string above. Output the value to the screen.
+
+
+# Using the string above, invoke a method to create a new string 
+# with all characters in UPPERCASE. Output the value to the screen.
+
+puts "Stallone".length 
+
+puts "Stallone".upcase 
