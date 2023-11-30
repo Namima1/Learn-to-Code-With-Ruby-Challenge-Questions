8e37481748973fb01d49219f75b860d0d458fa0d
Namima1 <129132573+Namima1@users.noreply.github.com>
2023-11-30 15:26:50 -0500

    Add files via upload

---
+++ section_7_intro_to_blocks.rb
@@ -0,0 +1,31 @@
+# Define a print_five_times method that accepts no arguments.
+# It should output the text "Hello" 5 times using the times method.
+# Use the print method in your solution.
+
+def print_five_times
+  5.times { puts "Hello" }
+end 
+
+print_five_times
+
+# Examples
+#
+# print_five_times() should output "HelloHelloHelloHelloHello"
+
+
+# Define a money_printer method that accepts a single numeric argument.
+# The number specifies how many times the method should print the
+# string "Money" in sequence. Use the 'print' method in your solution.
+
+#
+# Examples
+#
+# money_printer(3) should output MoneyMoneyMoney
+# money_printer(5) should output MoneyMoneyMoneyMoneyMoney
+# money_printer(0) should output nothing
+
+def money_printer(value) 
+  value.times { print "Money"}
+end 
+
+money_printer(3)
